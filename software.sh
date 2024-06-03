8cb09018d9c049dd028e95ab78de326e17014b40
Stefan <stefanwidmann@gmx.de>
2024-06-03 07:20:29 +0200

    Add configureation of nordvpn

--- software.sh
+++ software.sh
@@ -8,6 +8,11 @@ sudo systemctl start vnstat.service
 
 yay -S yakuake docker fish keepassxc visual-studio-code-bin nodejs npm flameshot snapper linux-headers vnstat tlp tlpui onedrive-abraunegg onedrivegui-git discord portfolio-performance-bin btop tldr neovim frpintd firefox chromium autorandr jetbrains-toolbox yt-dlp vlc fast gwenview okular nordvpn gimp jdownloader2 mediathekview less kimageformats5 tldr atuin
 
+#configure nordvpn
+sudo systemctl enable nordvpnd.service
+sudo systemctl start nordvpnd.service
+sudo usermod -aG nordvpn $USER
+
 # configure docker
 sudo systemctl enable docker
 sudo usermod -aG docker "$(whoami)"
